30b432fc49a7d99f09481e9f1fcd8b7168bcc02d
alkis axos <alkisaxos@gmail.com>
2023-11-30 20:35:46 +0200

    Home complete

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,11 +4,23 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div classname="header">
-      <header className="h1">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>How to... execute a bomb</p>
-      </header>
+    <div>
+      <p className="title">How to... execute a bomb</p>
+      <img src={logo} className="App-logo" alt="logo" />
+      <a href="/game1">
+        <div className="header">
+          <buttorn className="title">Chemical</buttorn>
+        </div>
+        <div className="header">
+          <buttorn className="title">Electrical</buttorn>
+        </div>
+        <div className="header">
+          <buttorn className="title">Radioenergy</buttorn>
+        </div>
+        <div className="header">
+          <buttorn className="title">Genetics</buttorn>
+        </div>
+      </a>
     </div>
   );
 }
